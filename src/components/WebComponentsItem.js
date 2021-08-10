b7415ae38a6716cf7bce038a62e04b25b6232582
juan carlos buendia <juan.carlosutninfo@gmail.com>
2021-08-10 15:23:00 -0500

    se actualizo esta clase

--- src/components/WebComponentsItem.js
+++ src/components/WebComponentsItem.js
@@ -8,6 +8,7 @@ const Item = ({title, score, overview, backdrop}) => (
     <div className="overlay">
       <div className="title">{title}</div>
       <div className="rating">{score} / 10</div>
+       <div className="rating">{score} / 10</div>
       <div className="plot">{overview}</div>
     </div>
   </div>
